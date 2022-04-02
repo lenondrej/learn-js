9a0f581c47c5cf5483f1baefb90b30f0cfc7a21c
Ondrej Mockor <78723474+lenondrej@users.noreply.github.com>
2022-04-02 09:25:03 +0200

    first lesson

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ obj = {
   "position": "head of this fucking world"
 }
 
-str = JSON.stringify(obj.name)
+str = JSON.stringify(obj.position)
 console.log(str)
 
 // const ahoj = 5;
 // ahoj=7;
+
+// add event listener
+// addBtn.addEventListener('click', someFunctionName);
+
+// parseInt the same is to use '+' sign
+let stringNumber = '10';
+console.log(parseInt(stringNumber));
+console.log(+stringNumber);
+
+// similar is converting nuber to string by using .toString()
+
+let number = 1000;
+console.log(typeof number.toString());
